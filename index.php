9f9aa2a18f567f7fe0eeeaf60d8330ef7c2fd0b9
Tiffany E Johnson unknown student <tej2@afsconnect2.njit.edu>
2017-11-19 18:45:27 -0500

    accounts

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+
+ini_set ('display_errors', 'On');
+error_reporting(E_ALL);
+
+define('DATABASE','tej2');
+define('USERNAME','tej2');
+define('PASSWORD','sCUGMmHv');
+define('CONNECTION','sql1.njit.edu');
+
+class Manage {
+	public static function autoload($class) {
+	include $class . '.php';
+	}
+}
+
+spl_autoload_register(array('Manage','autoload'));
+
+$obj = new main();
+
+class main
+{
+	public function __construct() {
+	$form = '<form method ="post" enctype="multipart/form-data">';
+	$form .= '<b>Table</b> <i>Accounts</i>';
+	$form .= 'Select all records';
+	$records = accounts::findAll();
+	$tableGen = displayTable::generateTableFromMultiArray($records);
+	$form .= $tableGen;
+	
+	$form .= 'Select one record';
+	$id = 4;
+	$records = accounts::findOne($id);
+	$tableGen = displayTable::generateTableFromOneRecord($records);
+	$form .= '<i>Retrieved record '.$id.'</i>';
+	$form .= $tableGen;
+
+	$form .= 'Insert one record';
+	$record = new account();
+	$record->email="tej2@njit.edu";
+	$record->fname="Tiffany";
+	$record->lname="Johnson";
+	$record->phone="983-989-9889";
+	$record->gender="female";
+	$record->pasword="9876578";
+	$lastInsertesId=$record->save();
+	$records = accounts::findAll();
+	$tableGen = displayTable::generateTableFromMultiArray($records);
+	$form .= '<i>Inserted</i>';
+	$form .= $tableGen;
+
+        $form .= 'Update one record';
+        $records = accounts::findOne($lastInsertedId);
+        $record = new account();
+        $record->id=$records->id;
+        $record->password="09877";
+        $record->save();
+        $form .= '<i>Updated id '.$records->id.'</i>';
+        $records = accounts::findAll();
+        $tableGen = htmlTable::genarateTableFromMultiArray($records);
+        $form .= $tableGen;
+
+        $form .= 'Delete one record';
+        $records = accounts::findOne($lastInsertedId);
+        $record= new account();
+        $record->id=$records->id;
+        $record->delete();
+	$form .= '<i>Record '.$records->id.' deleted</i>';
+	$records = accounts::findAll();
+        $tableGen = htmlTable::genarateTableFromMultiArray($records);
+	$form .= $tableGen;
+
+
+
+
