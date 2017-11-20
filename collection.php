6376f1aedbc3df7992cdc3b9f2c8520691028e23
Tiffany E Johnson unknown student <tej2@afsconnect2.njit.edu>
2017-11-19 21:54:52 -0500

    edits

--- collection.php
+++ collection.php
@@ -3,8 +3,6 @@
 abstract class collection {
     static public function create() {
         $model = new static::$modelName;
-        return $formmodel;
-        $model = new static::$modelName;
         return $model;
 	}
     static public function findAll() {
