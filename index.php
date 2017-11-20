2d425c133ef88f108c48ad4cc7baed4571bc944a
tej2 <31868523+tej2@users.noreply.github.com>
2017-11-19 22:22:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,144 @@ define('USERNAME','tej2');
 define('PASSWORD','sCUGMmHv');
 define('CONNECTION','sql1.njit.edu');
 
+
 class Manage {
-	public static function autoload($class) {
-	include $class . '.php';
-	}
+    public static function autoload($class) {
+        include $class . '.php';
+    }
 }
-
-spl_autoload_register(array('Manage','autoload'));
-
-$obj = new main();
+spl_autoload_register(array('Manage', 'autoload'));
+$obj=new main();
+class dbConn{
+    protected static $db;
+    private function __construct() {
+        try {
+            self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
+            self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        } catch (PDOException $e) {
+            echo "Connection Error: " . $e->getMessage();
+        }
+    }
+    public static function getConnection() {
+        if (!self::$db) {
+            new dbConn();
+        }
+        return self::$db;
+    }
+}
+abstract class collection {
+    protected $html;
+    static public function create() {
+        $model = new static::$modelName;
+        return $model;
+    }
+    static public function findAll() {
+        $db = dbConn::getConnection();
+        $tableName = get_called_class();
+        $sql = 'SELECT * FROM ' . $tableName;
+        $statement = $db->prepare($sql);
+        $statement->execute();
+        $class = static::$modelName;
+        $statement->setFetchMode(PDO::FETCH_CLASS, $class);
+        $recordsSet =  $statement->fetchAll();
+        return $recordsSet;
+    }
+    static public function findOne($id) {
+        $db = dbConn::getConnection();
+        $tableName = get_called_class();
+        $sql = 'SELECT * FROM ' . $tableName . ' WHERE id =' . $id;
+        $statement = $db->prepare($sql);
+        $statement->execute();
+        $class = static::$modelName;
+        $statement->setFetchMode(PDO::FETCH_CLASS, $class);
+        $recordsSet =  $statement->fetchAll();
+        return $recordsSet[0];
+    }
+}
+class accounts extends collection {
+    protected static $modelName = 'account';
+}
+class todos extends collection {
+    protected static $modelName = 'todo';
+}
+abstract class model {
+    protected $tableName;
+    public function save(){
+        if ($this->id != '') {
+            $sql = $this->update();
+        } else {
+           $sql = $this->insert();
+        }
+        $db = dbConn::getConnection();
+        $statement = $db->prepare($sql);
+        $array = get_object_vars($this);
+        foreach (array_flip($array) as $key=>$value){
+            $statement->bindParam(":$value", $this->$value);
+        }
+        $statement->execute();
+        $id = $db->lastInsertId();
+        return $id;
+    }
+    private function insert() {      
+        $modelName=get_called_class();
+        $tableName = $modelName::getTablename();
+        $array = get_object_vars($this);
+        $columnString = implode(',', array_flip($array));
+        $valueString = ':'.implode(',:', array_flip($array));
+        $sql =  'INSERT INTO '.$tableName.' ('.$columnString.') VALUES ('.$valueString.')';
+        return $sql;
+    }
+    private function update() {  
+        $modelName=get_called_class();
+        $tableName = $modelName::getTablename();
+        $array = get_object_vars($this);
+        $comma = " ";
+        $sql = 'UPDATE '.$tableName.' SET ';
+        foreach ($array as $key=>$value){
+            if( ! empty($value)) {
+                $sql .= $comma . $key . ' = "'. $value .'"';
+                $comma = ", ";
+                }
+            }
+            $sql .= ' WHERE id='.$this->id;
+        return $sql;
+    }
+    public function delete() {
+        $db = dbConn::getConnection();
+        $modelName=get_called_class();
+        $tableName = $modelName::getTablename();
+        $sql = 'DELETE FROM '.$tableName.' WHERE id ='.$this->id;
+        $statement = $db->prepare($sql);
+        $statement->execute();
+    }
+}
+class account extends model {
+    public $id;
+    public $email;
+    public $fname;
+    public $lname;
+    public $phone;
+    public $birthday;
+    public $gender;
+    public $password;
+    public static function getTablename(){
+        $tableName='accounts';
+        return $tableName;
+    }
+}
+class todo extends model {
+    public $id;
+    public $owneremail;
+    public $ownerid;
+    public $createddate;
+    public $duedate;
+    public $message;
+    public $isdone;
+    public static function getTablename(){
+        $tableName='todos';
+        return $tableName;
+    }
+} 
 
 class main
 {
@@ -24,13 +153,13 @@ class main
 	$form .= '<b>Table</b> <i>Accounts</i>';
 	$form .= 'Select all records';
 	$records = accounts::findAll();
-        $tableGen = displayTable::generateTableFromMultiArray($records);
+        $tableGen = displayHTML::displayTable($records);
 	$form .= $tableGen;
 	
 	$form .= 'Select one record';
 	$id = 4;
 	$records = accounts::findOne($id);
-	$tableGen = displayTable::generateTableFromOneRecord($records);
+	$tableGen = displayHTML::displayTableAlternate($records);
 	$form .= '<i>Retrieved record '.$id.'</i>';
 	$form .= $tableGen;
 
@@ -44,7 +173,7 @@ class main
 	$record->password="9876578";
 	$lastInsertedId=$record->save();
 	$records = accounts::findAll();
-	$tableGen = displayTable::generateTableFromMultiArray($records);
+	$tableGen = displayHTML::displayTable($records);
 	$form .= '<i>Inserted</i>';
 	$form .= $tableGen;
 
@@ -56,7 +185,7 @@ class main
         $record->save();
         $form .= '<i>Updated id '.$records->id.'</i>';
         $records = accounts::findAll();
-        $tableGen = displayTable::generateTableFromMultiArray($records);
+        $tableGen = displayHTML::displayTable($records);
         $form .= $tableGen;
 
         $form .= 'Delete one record';
@@ -66,19 +195,19 @@ class main
         $record->delete();
 	$form .= '<i>Record '.$records->id.' deleted</i>';
 	$records = accounts::findAll();
-        $tableGen = displayTable::generateTableFromMultiArray($records);
+        $tableGen = displayHTML::displayTable($records);
 	$form .= $tableGen;
 
 	$form .= '<b>Table</b> <i>Todos</i>';
 	$form .= 'Select all records';
 	$records = todos::findAll();
-	$tableGen = displayTable::generateTableFromMultiArray($records);
+	$tableGen = displayHTML::displayTable($records);
 	$form .= $tableGen;
 
 	$form .= 'Select one record';
 	$id = 3;
 	$records = todos::findOne($id);
-	$tableGen = displayTable::generateTableFromOneRecord($records);
+	$tableGen = displayHtml::displayTableAlternate($records);
 	$form .= '<i>Retrieved record '.$id.'</i>';
 	$form .= $tableGen;
 
@@ -92,7 +221,7 @@ class main
         $record->isdone=0;
         $lastInsertedId=$record->save();
 	$records = todos::findAll();
-	$tableGen = displayTable::generateTableFromMultiArray($records);
+	$tableGen = displayHTML::displayTable($records);
 	$form .= '<i>Inserted</i>';
 	$form .= $tableGen;
 
@@ -104,7 +233,7 @@ class main
         $record->save();
         $form .= '<i>Updated id '.$records->id.'</i>';
         $records = todos::findAll();
-        $tableGen = displayTable::generateTableFromMultiArray($records);
+        $tableGen = displayHTML::displayTable($records);
         $form .= $tableGen;
 
         $form .= 'Delete one record';
@@ -114,7 +243,7 @@ class main
         $record->delete();
 	$form .= '<i>Record '.$records->id.' deleted</i>';
         $records = todos::findAll();
-        $tableGen = displayTable::generateTableFromMultiArray($records);
+        $tableGen = displayHTML::displayTable($records);
         $form .= $tableGen;
 
         $form .= '</form> ';
